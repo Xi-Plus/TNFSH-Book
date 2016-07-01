50c492430590cdad575e5e19ea169de39e96a397
xiplus <huangxuanyuxiplus@gmail.com>
2016-07-01 21:34:12 +0800

    tinymce full featured

--- annc/index.php
+++ annc/index.php
@@ -50,7 +50,27 @@ require("../res/comhead.php");
 ?>
 <title>編輯公告-臺南一中教科書訂購系統</title>
 <script src="../res/js/tinymce/tinymce.min.js"></script>
-<script>tinymce.init({ selector:'textarea', language:'zh_TW' });</script>
+<script>
+tinymce.init({
+	selector: 'textarea',
+	language:'zh_TW',
+	height: 500,
+	theme: 'modern',
+	plugins: [
+		'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+		'searchreplace wordcount visualblocks visualchars code fullscreen',
+		'insertdatetime media nonbreaking save table contextmenu directionality',
+		'emoticons template paste textcolor colorpicker textpattern imagetools'
+	],
+	toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+	toolbar2: 'print preview media | forecolor backcolor emoticons',
+	image_advtab: true,
+	content_css: [
+		'//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+		'//www.tinymce.com/css/codepen.min.css'
+	]
+});
+</script>
 </head>
 <body Marginwidth="-1" Marginheight="-1" Topmargin="0" Leftmargin="0">
 <?php
